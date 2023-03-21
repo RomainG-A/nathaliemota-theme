c3debad26a810f437cf90da06041d06f57b9420c
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-21 23:18:49 +0100

    Mise en place modale de test

--- nathaliemotaphotos/front-page.php
+++ nathaliemotaphotos/front-page.php
@@ -1,3 +1,12 @@
 <?php get_header() ?>
+<main>
 
+<div id="myModal" class="modal">
+    <div class="modal-content">
+        <span class="close">x</span>
+        <p>Some text in the Modal..</p>
+    </div>
+</div>
+
+</main>
 <?php get_footer() ?>
