02e55bb277873e93e8716b536e670da16ec5459b
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 00:44:24 +0100

    Création du formulaire de contact

--- nathaliemotaphotos/front-page.php
+++ nathaliemotaphotos/front-page.php
@@ -12,8 +12,8 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
         </div>
     </div>
-    <div>
-        
+    <div class="modale__form">  	
+        <?php echo apply_shortcodes( '[contact-form-7 id="36" title="Formulaire de contact"]' ); ?> 
     </div>
 </div>
 
