e2e5c5b656004a07dd15e4a4aeaf14aa5a85ec2b
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 00:34:00 +0100

    Modale à la bonne taille

--- nathaliemotaphotos/front-page.php
+++ nathaliemotaphotos/front-page.php
@@ -1,10 +1,19 @@
 <?php get_header() ?>
 <main>
 
-<div id="myModal" class="modale">
-    <div class="modal-content">
-        <span class="close">x</span>
-        <p>Some text in the Modal..</p>
+<div class="modale">
+    <div class="modale__header">
+        <div class="modale__header__line1">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+        </div>
+        <div class="modale__header__line2">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+        </div>
+    </div>
+    <div>
+        
     </div>
 </div>
 
