ab3ff4afc764b05c44e7d1310b75a43fd5e2204f
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 17:35:28 +0100

    La modale s'ouvre mais se ferme au clic n'importe où

--- nathaliemotaphotos/assets/js/script.js
+++ nathaliemotaphotos/assets/js/script.js
@@ -1,30 +1,16 @@
 (function($) {
 'use strict';
 
-
-
 var modaleOuverte = false;
 
-function ouvrirModale() {
+$('.btn-modale').click(function() {
     $('.modale').css('display', 'block');
     modaleOuverte = true;
-}
-function fermerModale() {
-    $('.modale').css('display', 'none');
-    modaleOuverte = false; 
-}
-
-$('.btn-modale').click(function() {
-    if (!modaleOuverte) {
-        ouvrirModale();
-    }
-    else {
-        fermerModale();
-    }
 });
-$('main, footer').click(function() {
-    if (modaleOuverte) {
-        fermerModale();
+$('.modale').on('click', function(e) {
+    if (e.target != $('.modale__content') && modaleOuverte) {
+        $('.modale').css('display', 'none');
+        modaleOuverte = false;
     }
 });
 
