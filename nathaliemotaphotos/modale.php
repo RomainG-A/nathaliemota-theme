77fca694647eb19787746ba0769a911536be0d98
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 23:08:18 +0100

    Animation de l'header du formulaire

--- nathaliemotaphotos/modale.php
+++ nathaliemotaphotos/modale.php
@@ -1,12 +1,14 @@
 <div class="modale">
     <div class="modale__content">
         <div class="modale__content__header">
-            <div class="modale__content__header__line">
+            <div class="modale__content__header__line modale__content__header__line--line1">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
             </div>
-            <div class="modale__content__header__line">
+            <div class="modale__content__header__line modale__content__header__line--line2">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
