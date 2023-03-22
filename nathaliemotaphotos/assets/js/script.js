fec15855c3478a95fbb2db051eeefaf088d986bf
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 16:42:02 +0100

    Améliorarion structure et affichage modale

--- nathaliemotaphotos/assets/js/script.js
+++ nathaliemotaphotos/assets/js/script.js
@@ -4,16 +4,13 @@
 
 
 var modaleOuverte = false;
-var contenuSite = "header, main, footer";
 
 function ouvrirModale() {
     $('.modale').css('display', 'block');
-    $(contenuSite).addClass('darken');
     modaleOuverte = true;
 }
 function fermerModale() {
     $('.modale').css('display', 'none');
-    $(contenuSite).removeClass('darken');
     modaleOuverte = false; 
 }
 
--- nathaliemotaphotos/modale.php
+++ nathaliemotaphotos/modale.php
@@ -1,17 +1,19 @@
 <div class="modale">
-    <div class="modale__header">
-        <div class="modale__header__line">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+    <div class="modale__content">
+        <div class="modale__content__header">
+            <div class="modale__content__header__line">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+            </div>
+            <div class="modale__content__header__line">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+            </div>
         </div>
-        <div class="modale__header__line">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
+        <div class="modale__content__form">  	
+            <?php echo apply_shortcodes( '[contact-form-7 id="36" title="Formulaire de contact"]' ); ?> 
         </div>
-    </div>
-    <div class="modale__form">  	
-        <?php echo apply_shortcodes( '[contact-form-7 id="36" title="Formulaire de contact"]' ); ?> 
     </div>
 </div>
