464e27c46d273d8b4dcffa796274a492d1147692
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 15:41:46 +0100

    Factorisation script

--- nathaliemotaphotos/assets/js/script.js
+++ nathaliemotaphotos/assets/js/script.js
@@ -1,22 +1,33 @@
 (function($) {
 'use strict';
 
+
+
 var modaleOuverte = false;
+var contenuSite = "header, main, footer";
+
+function ouvrirModale() {
+    $('.modale').css('display', 'block');
+    $(contenuSite).addClass('darken');
+    modaleOuverte = true;
+}
+function fermerModale() {
+    $('.modale').css('display', 'none');
+    $(contenuSite).removeClass('darken');
+    modaleOuverte = false; 
+}
 
 $('.btn-modale').click(function() {
     if (!modaleOuverte) {
-        $('.modale').css('display', 'block');
-        modaleOuverte = true;
+        ouvrirModale();
     }
     else {
-        $('.modale').css('display', 'none');
-        modaleOuverte = false; 
+        fermerModale();
     }
 });
 $('main, footer').click(function() {
     if (modaleOuverte) {
-        $('.modale').css('display', 'none');
-    modaleOuverte = false;
+        fermerModale();
     }
 });
 
--- nathaliemotaphotos/front-page.php
+++ nathaliemotaphotos/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
 <main>
-
+<h1>TEEEEEEEEST</h1>
 </main>
 <?php get_footer() ?>
