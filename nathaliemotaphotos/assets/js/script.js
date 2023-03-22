775e1ef5f061d52a3cd886131525a2509b7acf42
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 14:47:12 +0100

    Le formulaire s'ouvre et se ferme quand on clique sur Contact

--- nathaliemotaphotos/assets/js/script.js
+++ nathaliemotaphotos/assets/js/script.js
@@ -1,36 +1,17 @@
 (function($) {
 'use strict';
 
-// Get the modal
-//var modal = document.getElementById('myModal');
+var modaleOuverte = false;
 
-// Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-/* btn.onclick = function() {
-    modal.style.display = "block";
-} */
 $('.btn-modale').click(function() {
-    $('.modale').css('display', 'block');
-    /* $('.modale').css({
-        'opacity': '1'
-    }); */
-});
-
-// When the user clicks on <span> (x), close the modal
-/* span.onclick = function() {
-    modal.style.display = "none";
-} */
-
-// When the user clicks anywhere outside of the modal, close it
-/* window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (!modaleOuverte) {
+        $('.modale').css('display', 'block');
+        modaleOuverte = true;
     }
-} */
+    else {
+        $('.modale').css('display', 'none');
+        modaleOuverte = false; 
+    }
+});
 
 })(jQuery);
