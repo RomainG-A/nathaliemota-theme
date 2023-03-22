22baac0cd2ff225f8548f05c4113a42d4705fd2c
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-22 23:08:59 +0100

    OK

--- nathaliemotaphotos/modale.php
+++ nathaliemotaphotos/modale.php
@@ -1,13 +1,13 @@
 <div class="modale">
     <div class="modale__content">
         <div class="modale__content__header">
-            <div class="modale__content__header__line modale__content__header__line--line1">
+            <div class="modale__content__header__line">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
             </div>
-            <div class="modale__content__header__line modale__content__header__line--line2">
+            <div class="modale__content__header__line">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact-texte.png" alt="Contact" />
