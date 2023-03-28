526f03c2e23e89125af558535e9c1dccbd5a5cb1
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-03-28 02:10:32 +0200

    Factorisation du style des colonnes avec Sass

--- nathaliemotaphotos/single-photos.php
+++ nathaliemotaphotos/single-photos.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-    <div class="page-photo">
+    <div class="page-photo bloc-page">
 
-        <section class="bloc-photo">
-            <div class="bloc-photo__description">
+        <section class="bloc-photo colonnes">
+            <div class="bloc-photo__description colonne">
                 <h1><?php the_title() ?></h1>
                 <p>Référence : <span id="reference-photo"><?php afficherChamp('reference_photo') ?></span></p>
                 <p>Catégorie : <?php afficherTaxonomie('categories'); ?></p>
@@ -11,10 +11,10 @@
                 <p>Type : <?php afficherChamp('type_photo') ?></p>
                 <p>Année : <?php echo get_the_date('Y');?></p>
             </div>
-            <img class="bloc-photo__image" src="<?php the_post_thumbnail_url(); ?>">
+            <img class="bloc-photo__image colonne" src="<?php the_post_thumbnail_url(); ?>">
         </section>
 
-        <section class="interaction-photo">
+        <section class="interaction-photo colonnes">
             <div>
                 <p>Cette photo vous intéresse ?</p>
                 <input class="interaction-photo__btn bouton btn-modale" type="button" value="Contact">
@@ -30,9 +30,9 @@
 
         <section class="recommandations">
             <h2>Vous aimerez aussi</h2>
-            <div class="recommandations__images">
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="Image similaire">
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="Image similaire">
+            <div class="recommandations__images colonnes">
+                <img class="colonne" src="<?php the_post_thumbnail_url(); ?>" alt="Image similaire">
+                <img class="colonne" src="<?php the_post_thumbnail_url(); ?>" alt="Image similaire">
             </div>
             <input class="recommandations__btn bouton" type="button" value="Toutes les photos">
         </section>
